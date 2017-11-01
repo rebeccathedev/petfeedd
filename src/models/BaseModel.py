248003349c60824ecae5313fd09fe360995a831a
Rebecca Peck <me@rebeccapeck.org>
2017-11-01 13:36:47 -0500

    Add timestamps to models.

--- src/models/BaseModel.py
+++ src/models/BaseModel.py
@@ -1,3 +1,5 @@
+import datetime
+
 from peewee import *
 from playhouse.sqlite_ext import SqliteExtDatabase
 from playhouse.shortcuts import model_to_dict, dict_to_model
@@ -5,6 +7,19 @@ from playhouse.shortcuts import model_to_dict, dict_to_model
 db = SqliteExtDatabase('petfeedd.db')
 
 class BaseModel(Model):
+    date_created = DateTimeField()
+    date_updated = DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if self._get_pk_value() is None:
+            # this is a create operation, set the date_created field
+            self.date_created = datetime.datetime.now().strftime(
+                "%Y-%m-%d %H:%M:%S")
+
+        self.date_updated = datetime.datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S")
+        return super(BaseModel, self).save(*args, **kwargs)
+
     def dict(self):
         return model_to_dict(self)
 
