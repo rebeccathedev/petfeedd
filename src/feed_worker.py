a44df8f5fd8508827f2aa8ff75f3e5c9a7a6418b
Rob Peck <rob@robpeck.com>
2017-11-01 13:20:38 -0500

    Implement a queue for messaging. Implement FeedEvent for tracking feeds.

--- src/feed_worker.py
+++ src/feed_worker.py
@@ -1,6 +1,22 @@
 import sqlite3
 import time
+import threading
+import queue
 
-def feed_worker():
-    while True:
-        time.sleep(10)
+class FeedWorker(threading.Thread):
+    def __init__(self, feed_queue, *args, **kwargs):
+        self.feed_queue = feed_queue
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        print("Starting feed worker.")
+        
+        while True:
+            try:
+                feed_event = self.feed_queue.get(timeout=3)
+                print(feed_event)
+                self.feed_queue.task_done()
+            except queue.Empty:
+                pass
+
+            time.sleep(1)
