80d9ebc0e352938d2e413e986a997802805a820d
Rob Peck <rob@robpeck.com>
2017-11-01 08:58:21 -0500

    Mostly documentation.

--- src/web_worker.py
+++ src/web_worker.py
@@ -33,13 +33,20 @@ class MyEncoder(json.JSONEncoder):
 
         return json.JSONEncoder.default(self, obj)
 
+# Create an instance of our custom Flask object.
 app = CustomFlask(__name__)
 
+# Define the web worker. This is the function that is passed to the thread to
+# start Flask.
 def web_worker():
+
+    # The main route returns the index page. Everything else is handled by the
+    # API and Vue.js.
     @app.route('/')
     def index():
         return render_template('index.html')
 
+    # A route that gets or updates settings.
     @app.route('/api/settings', methods=['GET', 'POST'])
     def settings():
         if request.method == 'GET':
@@ -47,6 +54,8 @@ def web_worker():
         else:
             return post_settings()
 
+    # A route that gets all feeds. In RESTful fashion, posting to this endpoint
+    # creates a new feed entry.
     @app.route('/api/feeds', methods=['GET', 'POST'])
     def feeds():
         if request.method == 'GET':
@@ -54,17 +63,26 @@ def web_worker():
         else:
             return post_feeds()
 
+    # A route that gets/updates/deletes feeds.
     @app.route('/api/feeds/<int:feed_id>', methods=['GET', 'POST', 'DELETE'])
     def feed(feed_id):
         if request.method == 'GET':
-            pass
+            return get_feed(feed_id)
         elif request.method == 'DELETE':
             return delete_feed(feed_id)
         else:
-            return post_feed()
+            return post_feed(feed_id)
 
+    # A route that pushes an immediate feed into the stack.
+    @app.route('/api/feed', methods=['POST'])
+    def feed_now():
+        pass
+
+    # Start the built-in Flask server.
     app.run("127.0.0.1", 8080)
 
+
+# This function gets all settings.
 def get_settings():
     settings = {}
     for setting in Setting.select():
@@ -72,10 +90,12 @@ def get_settings():
 
     return jsonify(settings)
 
+# This function updates settings.
 def post_settings():
     print(request.get_json())
     return get_settings()
 
+# This function gets all feeds.
 def get_feeds():
     feeds = []
     for feed in Feed.select().dicts():
@@ -83,18 +103,24 @@ def get_feeds():
 
     return json.dumps(feeds, cls=MyEncoder)
 
+# This function creates a new feed.
 def post_feeds():
     data = request.get_json(force=True)
     Feed.create(time=data["time"], name=data["name"], size=data["size"], last_feed=data["last_feed"], feed_count=data["feed_count"])
     return get_feeds()
 
-def get_feed():
-    pass
+# This function gets a specific feed.
+def get_feed(feed_id):
+    feed = Feed.select().where(Feed.id == feed_id).get()
+    if feed:
+        return json.dumps(feed.dict(), cls=MyEncoder)
 
-def post_feed():
+    return "{}"
+
+# This function updates a specific feed.
+def post_feed(feed_id):
     data = request.get_json(force=True)
     feed = Feed.select().where(Feed.id == data["id"]).get()
-    print(json.dumps(feed.dict(), cls=MyEncoder))
     if feed:
         feed.time = data["time"];
         feed.name = data["name"];
@@ -105,6 +131,7 @@ def post_feed():
 
     return json.dumps(feed.dict(), cls=MyEncoder)
 
+# This function deletes a specific feed.
 def delete_feed(feed_id):
     feed = Feed.select().where(Feed.id == feed_id).get()
     if feed:
