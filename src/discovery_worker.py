d378f019c40c9beab99dd753ede7dc4697e78de4
Rob Peck <rob@robpeck.com>
2017-11-01 14:49:08 -0500

    Implement an auto discovery system.

---
+++ src/discovery_worker.py
@@ -0,0 +1,17 @@
+import threading
+import socket
+
+class DiscoveryWorker(threading.Thread):
+
+    def run(self):
+        print("Starting auto discovery worker.")
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(("", 11213))
+
+        while True:
+            data, address = sock.recvfrom(1024)
+
+            if data:
+                print("Discovery request received from " + address[0])
+                sent = sock.sendto(b'petfeedd', address)
