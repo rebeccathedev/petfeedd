e3fca26926124402cd76ff73054f33ce21e2ed17
Rob Peck <rob@robpeck.com>
2020-08-07 21:37:26 -0500

    Update models to new format.

--- src/petfeedd-node/Models/Setting.js
+++ src/petfeedd-node/Models/Setting.js
@@ -1,8 +1,18 @@
-const Sequelize = require("sequelize");
+const { Sequelize, Model } = require("sequelize");
 
 module.exports = function(sequelize) {
-  return sequelize.define('settings', {
+  class Setting extends Model {}
+
+  Setting.init({
     key: Sequelize.STRING,
     value: Sequelize.STRING,
+  }, {
+    sequelize,
+    modelName: "setting",
+    tableName: "setting",
+    updatedAt: "date_updated",
+    createdAt: "date_created"
   });
+
+  return Setting
 }
